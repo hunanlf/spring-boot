368c66f6265ebf58f7a2031c7c1349b740b5638d
lf <2876207030@qq.com>
2019-01-26 11:56:51 +0800

    提交kotlin嵌入cas依赖的配置文件

---
+++ test-security/src/main/kotlin/com/yf/testsecurity/TestSecurityApplication.kt
@@ -0,0 +1,10 @@
+package com.yf.testsecurity
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class TestSecurityApplication
+
+fun main(args: Array<String>) {
+//    runApplication<TestSecurityApplication>(*args)
+}
---
+++ test-security/src/test/kotlin/com/yf/testsecurity/TestSecurityApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.yf.testsecurity
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class TestSecurityApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
